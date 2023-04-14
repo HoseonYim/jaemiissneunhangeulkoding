3f307c0806859d6d482b4090a05d6ee80d341b27
Houseon Yim <justin.yim14@gmail.com>
2023-04-14 11:00:57 +0900

    Create version3.c

---
+++ version3.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define 메인 main
+
+#define 출력 printf
+#define 입력 scanf
+
+#define 등호 =
+#define 왼쪽으로벌린상어입 >
+#define 오른쪽으로벌린상어입 <
+
+#define 더블등호 ==
+#define 왼쪽으로벌린상어입과등호 >=
+#define 오른쪽으로벌린상어입과등호 <=
+#define 느낌표등호 !=
+
+#define 앞소괄호 (
+#define 뒤소괄호 )
+#define 앞중괄호 {
+#define 뒤중괄호 }
+#define 앞대괄호 [
+#define 뒤대괄호 ]
+
+#define 공허 void
+#define 정수 int
+#define 실수 float
+#define 문자 char
+#define 큰실수 double
+#define 큰 long
+
+#define 반복 for
+#define 하는동안 while
+
+#define 만약 if
+#define 다른만약 else if
+#define 나머지만약 else
+
+#define 쉼표 ,
+#define 주소 &
+#define 세미클론 ;
+
+#define 절댓값 abs
+#define 제곱근 sqrt
+#define 제곱 pow
+
+#define 정수변수1 a
+#define 정수변수2 b
+#define 실수변수1 f
+#define 실수변수2 g
+#define 문자변수1 c
+#define 문자변수2 s
+
+#define 돌아가기 return
+
+#define 세상에서가장중요한숫자 0
+
+#define 더하기 +
+#define 빼기 -
+#define 곱하기 *
+#define 나누기 /
+#define 나머지 %
+
+정수 메인 앞소괄호 뒤소괄호 앞중괄호
+   
+뒤중괄호
